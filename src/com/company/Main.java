f09df70a3a7fa4cf6e26a3ea9f8c4b4e8d2e5131
JustinYates <Justin.yates24601@gmail.com>
2018-02-01 14:45:50 -0500

    little change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-     /*   Scanner Color = new Scanner(System.in);
+     /*   Scanner Color = new Scanner(System.in));
 
        Cat cat = new Cat(4,true, Color.nextLine());
         System.out.println(cat.getColor());*/
